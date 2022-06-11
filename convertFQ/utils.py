20af3c037b2899890e1e3550ce42b6d49d319924
Ahmad Nuruddin Khoiri <39895116+NuruddinKhoiry@users.noreply.github.com>
2022-06-11 09:14:19 +0700

    Update utils.py

--- convertFQ/utils.py
+++ convertFQ/utils.py
@@ -4,7 +4,6 @@ import sys
 
 def get_args():
     parser = ArgumentParser(description="convertFQ: RNA -> DNA | DNA -> RNA")
-    parser.add_argument("-h", "--help",action="help",help="show the help and exit")
     parser.add_argument("-v", "--version",help="Print version and exit.",action="version",version='ConvertFQ {}'.format(__version__))
     parser.add_argument("-i", "--input", required=True, help="Input FASTQ [Required]")
     parser.add_argument("-c", "--conversion", required=True, type=str, help="rna2dna | dna2rna [Required]")
